e22f0f4adcd934b4e5e1f0ff695cfa8d6ce2e6ca
Jeffrey <jeffrey.magmachu@gmail.com>
2017-05-29 22:51:40 -0400

    still random shit, experimenting

--- src/Console.kt
+++ src/Console.kt
@@ -1,42 +1,60 @@
+import java.awt.Font
 import java.awt.Toolkit
 import java.awt.datatransfer.DataFlavor
 import java.awt.event.KeyEvent
 import java.awt.event.KeyListener
+import java.util.*
 import javax.swing.JFrame
 import javax.swing.JPanel
 import javax.swing.WindowConstants
 
 class Console() : KeyListener {
-    val keyboard_buffer = listOf<Char>()
-    var frame = JFrame()
+    val keyboard_buffer = LinkedList<Char>()
+    lateinit var frame: JFrame
     val panel = JPanel()
+    lateinit var font: Font
 
 
+    companion object {
+        const val DEFAULT_ROWS = 25
+        const val DEFAULT_COLS = 80
+        const val DEFAULT_FONT_SIZE = 14
+        const val DEFAULT_TITLE = "Console"
+    }
+
+    // TODO: this prints
     fun print(s: String) {
 
     }
 
+    fun getChar() = keyboard_buffer.poll()
+
+
     override fun keyTyped(e: KeyEvent?) {
-        // TODO check if int to char comparison works
-        when (e?.keyChar) {
+        if (e?.keyChar == null) return
+        val c: Char = e.keyChar
+        when (c) {
             21.toChar() -> System.exit(0) // Quit
             26.toChar() -> { // Paste
                 println("paste")
                 val clipboard: String = Toolkit.getDefaultToolkit().systemClipboard.getData(DataFlavor.stringFlavor) as String
-                for (c: Char in clipboard) {
-                    if (c in '!'..'_' || c == '\n') println(c)
+                for (ch: Char in clipboard) {
+                    if (font.canDisplay(ch)) keyboard_buffer.add(ch)
                 }
             }
-            in '!'..'_' -> keyboard_buffer.last()
+        }
+        if (font.canDisplay(c)) {
+            keyboard_buffer.add(c)
+            (this as Object).notify()
         }
     }
 
     override fun keyPressed(e: KeyEvent?) {}
     override fun keyReleased(e: KeyEvent?) {}
 
-    constructor(font_size: Int) : this(font = font_size)
+    constructor(font_size: Int) : this(DEFAULT_ROWS, DEFAULT_COLS, font_size)
 
-    constructor(rows: Int = 25, columns: Int = 80, font: Int = 24, title: String = "Console") : this() {
+    @JvmOverloads constructor(rows: Int, columns: Int, font_size: Int = DEFAULT_FONT_SIZE, title: String = DEFAULT_TITLE) : this() {
         frame = JFrame(title)
     }
 
--- src/Tester.java
+++ src/Tester.java
@@ -1,11 +1,9 @@
 import java.awt.*;
-import java.awt.datatransfer.DataFlavor;
-import java.awt.datatransfer.UnsupportedFlavorException;
-import java.io.IOException;
 
 public class Tester {
     public static void main(String[] args) {
-        Console c = new Console(14);
+        System.out.println(new Font("Tahoma", Font.PLAIN, 12).canDisplay('\25'));
+        Console c = new Console();
         c.print("asdf");
     }
 }
